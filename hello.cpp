b7a9c8b455f1857a7b57736d008edbb8c17afee5
Jan Henrik Weinstock <janweinstock@users.noreply.github.com>
2021-07-25 11:38:01 +0200

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -22,6 +22,6 @@ int main(int argc, char** argv) {
     if (argc > 1)
         std::cout << "Hello " << argv[1] << "!" << std::endl;
     else
-        std::cout << "Hello World!" << std::endl;
+            std::cout << "Hello World!" << std::endl;
     return 0;
 }
